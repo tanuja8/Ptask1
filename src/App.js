2cbbcd9ab8015eb97b108c639df10e069caf15c9
Tanuja <tanujakhatun292@gmail.com>
2023-08-07 02:05:21 +0530

    create a new component slider

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Fasion from './Component/Fasion';
+import Slide from './Component/Slide';
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Fasion/>
+    <Slide/>
     </div>
   );
 }
